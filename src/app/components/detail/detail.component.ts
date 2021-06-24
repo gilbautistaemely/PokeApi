b258fdbb30b9878fca2f6f9440682f318f1fe7c4
Emely Bautista GIl <emelybautistagil@hotmail.com>
2021-06-24 19:44:54 -0400

    Ultimo Commit

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-detail',
@@ -37,9 +38,24 @@ export class DetailComponent implements OnInit {
     )
   }
 
-  pdf(name) {
-    const doc = new jsPDF();
-    
+  pdf(name, height, weight, imagen, id,tipo) {
+     name = name.toUpperCase()
+    var doc = new jsPDF("p", "mm", "a4");
+    doc.setFont("Times-Roman","bold");
+    doc.setFontSize(40);
+    doc.text(`${name}`,70, 40,)
+    doc.setFontSize(20);
+    doc.text(`Características de ${name}`,50, 160)
+    doc.text(`Tipo de Pokemon: ${tipo}`,60, 180)
+    doc.text(`Altura: ${height}`,60, 190)
+    doc.text(`Ancho: ${weight}`,60, 200)
+    doc.addImage(imagen, 'JPEG', 55, 50, 100, 100);
+
+    doc.addImage(imagen, 'JPEG', 15, 30, 15, 15);
+    doc.addImage(imagen, 'JPEG', 180, 30, 15, 15);
+    doc.addImage(imagen, 'JPEG', 15, 200, 15, 15);
+    doc.addImage(imagen, 'JPEG', 180, 200, 15, 15);
+
     doc.save(`Pokemon ${name}`);
   }
 
